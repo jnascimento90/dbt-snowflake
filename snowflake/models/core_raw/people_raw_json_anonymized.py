82f8f45a9e3e916a8074bebef6d7e950638a241b
jnascimento90 <126112234+jnascimento90@users.noreply.github.com>
2023-06-26 11:18:12 +0100

    Add files via upload

---
+++ snowflake/models/core_raw/people_raw_json_anonymized.py
@@ -0,0 +1,17 @@
+import base64
+import pandas
+
+def encoding(x):
+    x = x.encode("ascii")
+    base64_bytes = base64.b64encode(x)
+    return base64_bytes.decode("ascii")
+    
+    
+def model(dbt, session):
+    dbt.config(materialized="table")
+    
+    df = dbt.source('public_public', 'people_raw_json_split').to_pandas()
+    df['FAMILY_NAME'] = df['FAMILY_NAME'].apply(encoding)
+    del df['GIVEN_NAME']
+
+    return df
