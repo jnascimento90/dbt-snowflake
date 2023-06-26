4369931ff51d324126cd04e4b6fb749a685983e5
jnascimento90 <126112234+jnascimento90@users.noreply.github.com>
2023-06-26 14:43:01 +0100

    Update people_raw_json_anonymized.py

--- snowflake/models/core_raw/people_raw_json_anonymized.py
+++ snowflake/models/core_raw/people_raw_json_anonymized.py
@@ -17,6 +17,6 @@ def model(dbt, session):
     # fix datetime
     #df["_AIRBYTE_EMITTED_AT"] = df["_AIRBYTE_EMITTED_AT"].div(1000000)
     #df["_AIRBYTE_EMITTED_AT"] = pd.to_datetime(df["_AIRBYTE_EMITTED_AT"],unit='s')
-    df["_AIRBYTE_EMITTED_AT"] = pd.to_datetime(df["_AIRBYTE_EMITTED_AT"],unit='us')
+    df["_AIRBYTE_EMITTED_AT"] = pandas.to_datetime(df["_AIRBYTE_EMITTED_AT"],unit='us')
 
     return df
