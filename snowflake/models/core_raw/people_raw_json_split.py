a713f23c01bfa8f0f8b11bc68969767f6801387c
jnascimento90 <126112234+jnascimento90@users.noreply.github.com>
2023-06-26 10:46:21 +0100

    Update people_raw_json_split.py

--- snowflake/models/core_raw/people_raw_json_split.py
+++ snowflake/models/core_raw/people_raw_json_split.py
@@ -6,7 +6,7 @@ from snowflake.snowpark.functions import col
 import json
 import pandas as pd
 
-def main(session: snowpark.Session):
+def model(session: snowpark.Session):
     
     tableName = 'public._AIRBYTE_RAW_PEOPLE_RAW'
     dataframe = session.table(tableName)
