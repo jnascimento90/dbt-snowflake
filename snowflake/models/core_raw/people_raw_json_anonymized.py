a7ed8afa33b35b216b63272788afb2dc884b54a6
jnascimento90 <126112234+jnascimento90@users.noreply.github.com>
2023-06-29 10:59:08 +0100

    Update people_raw_json_anonymized.py

--- snowflake/models/core_raw/people_raw_json_anonymized.py
+++ snowflake/models/core_raw/people_raw_json_anonymized.py
@@ -18,6 +18,7 @@ def model(dbt, session):
     #df["_AIRBYTE_EMITTED_AT"] = pandas.to_datetime(df["_AIRBYTE_EMITTED_AT"],unit='us')
     df["_AIRBYTE_EMITTED_AT"] = df["_AIRBYTE_EMITTED_AT"].dt.tz_localize('UTC')
 
-    session.write_pandas(df, 'PEOPLE_RAW_JSON_SPLIT')
+    #session.write_pandas(df, 'PEOPLE_RAW_JSON_SPLIT')
+    df.to_sql('PEOPLE_RAW_JSON_SPLIT', con=session, if_exists='replace')
     
     return df
